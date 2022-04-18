ff4fa160af675fcc6e0312a5745ef688690cb063
ester <95254487+esterrpereira@users.noreply.github.com>
2022-04-18 19:09:56 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+  float num=0, i=0, soma=0,media;
+  
+
+ 
+  
+  while (num>=0){
+ printf("Digite alguns números  (obs: digite um número negativo para parar) :  ");
+   scanf("%f",&num);
+  
+    soma=soma +num;
+    i++;
+  }
+  media = soma/i;
+  printf("Media = %f", media);
+  
+}
